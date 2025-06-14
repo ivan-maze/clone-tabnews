16afb6c3a80746ef086935ca2276e5b5e17034b1
Ivan Ramos Biagioni <140509979+ivan-maze@users.noreply.github.com>
2025-06-14 20:49:09 +0000

    testing erros during `npm-run-dev` with new scripts

---
+++ infra/scripts/wait-for-postgres.js
@@ -0,0 +1,17 @@
+const { exec } = require("node:child_process");
+
+function checkPostgress() {
+  exec("docker exec postgres-dev pg_isready", handleReturn);
+
+  function handleReturn(error, stdout) {
+    if (stdout.search("accepting connections") === -1) {
+      process.stdout.write(".");
+      checkPostgress();
+      return;
+    }
+    console.log("\n🟢 Postgres está pronto e aceitando conexões!\n");
+  }
+}
+
+process.stdout.write("\n\n🔴 Aguardando Postgress aceitar conexões");
+checkPostgress();
